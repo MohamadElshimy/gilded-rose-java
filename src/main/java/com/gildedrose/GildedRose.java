c48a7e02c7957681ce77b21d5c9890c2f9acf945
Mohamad <mohamad.elshimy.ext@nokia.com>
2024-09-03 19:05:18 +0300

    refactoring to clean code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -3,60 +3,82 @@ package com.gildedrose;
 class GildedRose {
     Item[] items;
 
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        for (Item item : items) {
+            if (item.name.equals(SULFURAS)) {
+                return;
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            decreaseSellIn(item);
+
+            switch (item.name) {
+                case AGED_BRIE:
+                    updateAgedBrie(item);
+                    break;
+                case BACKSTAGE_PASSES:
+                    updateBackstagePasses(item);
+                    break;
+                default:
+                    updateNormalItem(item);
+                    break;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if (item.sellIn < 0) {
+                handleExpiredItem(item);
             }
+        }
+    }
+
+    private void decreaseSellIn(Item item) {
+        item.sellIn -= 1;
+    }
+
+    private void updateAgedBrie(Item item) {
+        increaseQuality(item);
+    }
+
+    private void updateBackstagePasses(Item item) {
+        increaseQuality(item);
+
+        if (item.sellIn <= 10) {
+            increaseQuality(item);
+        }
+        if (item.sellIn <= 5) {
+            increaseQuality(item);
+        }
+    }
+
+    private void updateNormalItem(Item item) {
+        decreaseQuality(item);
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+    }
+
+    private void handleExpiredItem(Item item) {
+        if (item.name.equals(AGED_BRIE)) {
+            increaseQuality(item); // made this to increase by two as opposite to normal items (not 100% sure)
+        } else if (item.name.equals(BACKSTAGE_PASSES)) {
+            item.quality = 0;
+        } else {
+            decreaseQuality(item); // double quality degradation
         }
     }
 }
